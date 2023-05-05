a17ff080e0db6ea5784195492bda6760e61bc634
Vin Bui <vdb23@cornell.edu>
2023-05-05 11:04:08 -0400

    Convert to SwiftUI

---
+++ Savvy/SavvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavvyApp.swift
+//  Savvy
+//
+//  Created by Vin Bui on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SavvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
